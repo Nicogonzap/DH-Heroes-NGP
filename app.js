b081230a18e76ec024809b26166fa70eb308ee3d
Nicogonzap <nicolasgonzap@gmail.com>
2021-10-18 21:31:45 -0300

    agregada funcionalidad de port

--- app.js
+++ app.js
@@ -11,11 +11,11 @@
 const express = require('express');
 const app = express ();
 const path = require ('path');
-
+const port = 3030;
 app.use(express.static('public'));
 
 
-app.listen (3030, () => {
+app.listen (port, () => {
     console.log ('Servidor corriendo')
 })
 
